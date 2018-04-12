b0d702416f59ae2c333b7412f28c92d097b0c349
Douglas Wille <willedouglas10@gmail.com>
2018-04-12 16:27:07 -0300

    Adjsted cors

--- index.js
+++ index.js
@@ -21,38 +21,33 @@ catch(err) {
 process.env.appRoot = __dirname;
 process.env.certPath = path.join(__dirname, 'config/certificates');
 
-app.use(cors({ origin: '*' }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/', (req, res, next) => {
-  res.redirect(process.env.REDIRECT_URL);
-
-  return next();
+app.get('/', (req, res) => {
+  return res.redirect(process.env.REDIRECT_URL);
 });
 
-app.post('/login', (req, res, next) => {
+app.post('/login', (req, res) => {
   QRS.getTicket(req.body.username, (error, ticket) => {
     if (ticket) {
-      res.status(200).json({ status: true, result: { message: 'Autenticado com sucesso.', ticket: ticket.Ticket }});
-    } else {
-      res.status(500).send({ status: false, error: error });
+      return res.status(200).json({ status: true, result: { message: 'Autenticado com sucesso.', ticket: ticket.Ticket }});
     }
-
-    return next();
+    
+    return res.status(500).send({ status: false, error: error });
   })
 });
 
 app.delete('/logout/:session_user', (req, res, next) => {
   QRS.logout(req.params.session_user, (error, session) => {
     if (session) {
-      res.status(200).json({ status: true, result: { message: 'Você saiu do portal.', session: session.sessionId }});  
-    } else {
-      res.status(500).send({ status: false, error: error }); 
-    }
+      return res.status(200).json({ status: true, result: { message: 'Você saiu do portal.', session: session.sessionId }});  
+    } 
 
-    return next();
+    return res.status(500).send({ status: false, error: error }); 
   })
 });
+
+app.options('*', cors());
 
 app.listen(process.env.APPLICATION_PORT, () => { console.info('==> Listening on port %s. Open up http://0.0.0.0:%s/ in your browser.', process.env.APPLICATION_PORT, process.env.APPLICATION_PORT); });

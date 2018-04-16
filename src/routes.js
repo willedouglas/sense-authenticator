961395dfb53b5347e8e4dd78299fab6fe9e5cbe4
Douglas Wille <willedouglas10@gmail.com>
2018-04-16 17:57:59 -0300

    Another test

--- src/routes.js
+++ src/routes.js
@@ -17,9 +17,8 @@ module.exports = app => {
   app.post('/session/login', (req, res) => {
     QRS.createSession(req.body.username, (error, session) => {
       if (session) {
-        res.set('Set-Cookie', 'firstCookie=firstValue');
+        res.cookie('X-Qlik-Session-Hefesto', session.SessionId);
         return res.status(200).json({ status: true, result: { message: 'Sessão ' + session.SessionId + ' criada com sucesso.', session: session.SessionId }});
-        //return res.cookie('X-Qlik-Session-Hefesto', session.SessionId);
       }
       return res.status(500).json({ status: false, error: error });
     });

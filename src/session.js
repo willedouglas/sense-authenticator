67c2f1b3317732857c5d3a58bbe391a80c136da2
Douglas Wille <willedouglas10@gmail.com>
2018-04-16 20:29:42 -0300

    Test

--- src/session.js
+++ src/session.js
@@ -3,20 +3,19 @@ const cookieParser = require('cookie-parser');
 
 module.exports = app => {
 	app.use(cookieParser());
-	app.use(session({
-		secret: process.env.SENSE_PROXY,
-		resave: false,
-		saveUninitialized: true,
-		name: 'X-Qlik-Session-Hefesto1',
-		value: '123456789ABCEFGH',
-		genid: () => '123456789',
-		cookie: {
-			name: 'ADHJLSDJKSD2',
-			path: '/',
-			domain: process.env.SENSE_SERVER,
-			maxAge: 1000 * 60 * 24,
-		}
-	}));
+	// app.use(session({
+	// 	secret: process.env.SENSE_PROXY,
+	// 	resave: false,
+	// 	saveUninitialized: true,
+	// 	name: 'X-Qlik-Session-Hefesto',
+	// 	value: '123456789ABCEFGH',
+	// 	genid: () => '123456789',
+	// 	cookie: {
+	// 		path: '/',
+	// 		domain: process.env.SENSE_SERVER,
+	// 		maxAge: 1000 * 60 * 24,
+	// 	}
+	// }));
 
 	app.use(function (req, res, next) {
 		res.header('Access-Control-Allow-Credentials', true);

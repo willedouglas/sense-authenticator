b30e5a14a88719336e15d69b6cf8d402e0f4a272
Douglas Wille <willedouglas10@gmail.com>
2018-04-16 15:27:54 -0300

    cookies test

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ const QRS = require("./qrs");
 
 module.exports = app => { 
   app.get('/', (req, res) => {
+    res.cookie('name', 'express').send('cookie set');
     return res.redirect(process.env.REDIRECT_URL);
   });
 

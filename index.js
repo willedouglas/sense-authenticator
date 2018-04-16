c069f9a0165b86cf3020ee061d8a0c16e1dd3474
Douglas Wille <willedouglas10@gmail.com>
2018-04-16 19:58:15 -0300

    Adjusted sessions

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ const fs = require("fs");
 const path = require('path');
 const https = require('https');
 const app = require('express')();
-const headers = require('./src/headers');
+const session = require('./src/session');
 const routes = require('./src/routes');
 const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
 
 try {
   const config = require('./config/config');
@@ -22,10 +21,9 @@ catch(err) {
 process.env.appRoot = __dirname;
 process.env.certPath = path.join(__dirname, 'config/certificates');
 
-app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-headers(app);
+session(app);
 routes(app);
 
 app.listen(process.env.APPLICATION_PORT, () => { console.info('==> Listening on port %s. Open up http://0.0.0.0:%s/ in your browser.', process.env.APPLICATION_PORT, process.env.APPLICATION_PORT); });
---
+++ src/session.js
@@ -0,0 +1,22 @@
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+
+module.exports = app => { 
+  app.use(cookieParser());
+  app.use(session({
+      secret: process.env.SENSE_PROXY,
+      cookie: {
+          path: '/',
+          domain: process.env.SENSE_SERVER,
+          maxAge: 1000 * 60 * 24
+      }
+  }));
+
+  app.use(function(req, res, next) {
+      res.header('Access-Control-Allow-Credentials', true);
+      res.header('Access-Control-Allow-Origin', req.headers.origin);
+      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+      res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+      next();
+  });
+};

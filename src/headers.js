04e7b94357bc81e310d6c5292507ef090ad7850b
Douglas Wille <willedouglas10@gmail.com>
2018-04-16 22:12:13 -0300

    x-qlik-capabilities added

--- src/headers.js
+++ src/headers.js
@@ -3,7 +3,7 @@ module.exports = app => {
 		res.header('Access-Control-Allow-Credentials', true);
 		res.header('Access-Control-Allow-Origin', req.headers.origin);
 		res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-		res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+		res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, x-qlik-capabilities');
 		next();
 	});
 };
